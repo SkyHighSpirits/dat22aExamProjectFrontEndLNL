f57f2198c59e4a1d7720747378e428a8cc9911e3
Nicolai Ene <35382666+SkyHighSpirits@users.noreply.github.com>
2023-11-23 16:45:41 +0100

    Fix to make email sending work

--- javascript/Frontpage.js
+++ javascript/Frontpage.js
@@ -5,3 +5,32 @@ document.addEventListener("DOMContentLoaded", function () {
     callNavbarTemplate()
     callFooterTemplate()
 });
+
+let contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(contactForm);
+
+    const requestData = new URLSearchParams(formData);
+
+    fetch('http://localhost:8080/send-email', {
+        method: 'POST',
+        body: requestData,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    })
+        .then(response => {
+            if (response.ok) {
+                console.log('Email sent successfully!');
+            } else {
+                console.log(response);
+                console.log('Failed to send email.');
+            }
+        })
+        .catch(error => {
+            console.log('An error occurred:', error);
+        });
+});
