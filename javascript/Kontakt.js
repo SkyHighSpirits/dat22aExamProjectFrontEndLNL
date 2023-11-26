fe0efe4e88f98846e7d710559063fdcf91966489
Lasse Lysskov <56248206+Lasse11551@users.noreply.github.com>
2023-11-26 21:45:59 +0100

    deleteUnusedCode

--- javascript/Kontakt.js
+++ javascript/Kontakt.js
@@ -1,19 +1,36 @@
-import { callNavbarTemplate, callFooterTemplate, changeToKontakt, changeToForside, changeToYdelser, changeToPortefolje, changeToOmMig } from "./template.js";
+import { callNavbarTemplate, callFooterTemplate } from "./template.js";
 
 document.addEventListener("DOMContentLoaded", function () {
     callNavbarTemplate()
     callFooterTemplate()
 });
 
-const pbForsideButton = document.getElementById("pbForsideButton")
-const pbPortefoljeButton = document.getElementById("pbPortefoljeButton")
-const pbYdelserButton = document.getElementById("pbYdelserButton")
-const pbOmMigButton = document.getElementById("pbOmMigButton")
-const pbKontaktButton = document.getElementById("pbKontaktButton")
+// Hvis der er en kontaktformular på forsiden, skal du tilføje event listener til den.
+let contactForm = document.getElementById("contact-form");
+if (contactForm) {
+    contactForm.addEventListener("submit", function (event) {
+        event.preventDefault();
 
+        const formData = new FormData(contactForm);
+        const requestData = new URLSearchParams(formData);
 
-pbForsideButton.addEventListener('click', changeToForside)
-pbPortefoljeButton.addEventListener('click', changeToPortefolje)
-pbYdelserButton.addEventListener('click', changeToYdelser)
-pbOmMigButton.addEventListener('click', changeToOmMig)
-pbKontaktButton.addEventListener('click', changeToKontakt)
+        fetch('http://localhost:8080/send-email', {
+            method: 'POST',
+            body: requestData,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        })
+            .then(response => {
+                if (response.ok) {
+                    console.log('Email sent successfully!');
+                } else {
+                    console.log(response);
+                    console.log('Failed to send email.');
+                }
+            })
+            .catch(error => {
+                console.log('An error occurred:', error);
+            });
+    });
+}
