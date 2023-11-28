6edac3ed1d53fe15b297208d9d2778561cec47e1
Nicolai Ene <35382666+SkyHighSpirits@users.noreply.github.com>
2023-11-28 10:25:51 +0100

    about js

--- javascript/OmMig.js
+++ javascript/OmMig.js
@@ -1,6 +1,32 @@
 import { callNavbarTemplate, callFooterTemplate } from "./template.js";
 
+let aboutInformation = null;
+
 document.addEventListener("DOMContentLoaded", function () {
     callNavbarTemplate();
     callFooterTemplate();
+    fetchData();
+
+
 });
+
+async function fetchData() {
+    try {
+        const response = await fetch("http://localhost:8080/company")
+
+        const data = await response.json();
+        if (response.ok)
+        {
+            aboutInformation = data;
+        }
+        else
+        {
+            console.log ("Could not fetch data")
+        }
+        console.log(data);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+
