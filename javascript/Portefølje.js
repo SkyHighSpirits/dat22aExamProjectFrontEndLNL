163000759e7b98f1342c6aea6ed11e336066b0eb
Xarantaur <115454990+Xarantaur@users.noreply.github.com>
2023-11-30 13:02:19 +0100

    style af porteføjle
    og opdatering af javascript

--- javascript/Portefølje.js
+++ javascript/Portefølje.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function hentYdelser() {
-    fetch('http://localhost:8083/api/operations')
+    fetch('http://localhost:8080/api/operations')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Netværksrespons var ikke ok');
@@ -35,7 +35,7 @@ function opdaterDropdown(data) {
 
 function hentPorteføljeEmner() {
     // Ændring foretaget her: Antallet af emner pr. side sat til 3
-    fetch(`http://localhost:8083/getPosts?page=${currentPage}&size=3`)
+    fetch(`http://localhost:8080/getPosts?page=${currentPage}&size=3`)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Netværksrespons var ikke ok');
@@ -56,28 +56,62 @@ function opdaterPortefølje(data) {
         const emneDiv = document.createElement('div');
         emneDiv.classList.add('porteføljeEmne');
 
+
+        const infoCon = document.createElement("div")
+        infoCon.classList.add('infoCon')
+        emneDiv.appendChild(infoCon)
+
+        const titelDiv = document.createElement("div")
+        titelDiv.classList.add('porteføljeTitel')
+        titelDiv.textContent = item.poster.poster_Title
+        emneDiv.appendChild(titelDiv)
+
+
         const beskrivelseDiv = document.createElement('div');
         beskrivelseDiv.classList.add('porteføljeBeskrivelse');
         beskrivelseDiv.textContent = item.poster.poster_Description;
-        emneDiv.appendChild(beskrivelseDiv);
+        infoCon.appendChild(beskrivelseDiv);
 
         const billederDiv = document.createElement('div');
         billederDiv.classList.add('porteføljeBilleder');
         item.images.forEach(billede => {
             const img = document.createElement('img');
             img.src = 'data:image/jpeg;base64,' + billede.byte_img;
+            img.classList.add('clickable-image')
             billederDiv.appendChild(img);
         });
-        emneDiv.appendChild(billederDiv);
+        infoCon.appendChild(billederDiv);
 
         porteføljeContainer.appendChild(emneDiv);
+        emneDiv.appendChild(infoCon)
     });
 }
 
+function openLargeImagePopup(imageSrc) {
+    const popup = document.createElement('div');
+    popup.classList.add('image-popup');
+    popup.innerHTML = `<img src="${imageSrc}" alt="Large Image">`;
+
+    // Append the popup to the body
+    document.body.appendChild(popup);
+
+    // Add a click event listener to close the popup when clicked
+    popup.addEventListener('click', function () {
+        document.body.removeChild(popup);
+    });
+}
 // Funktion til at indlæse flere emner
 function loadMore() {
     hentPorteføljeEmner();
 }
+
+
+document.addEventListener('click', function (event) {
+    //hvis elemented har clickable-image class så :
+    if (event.target.classList.contains('clickable-image')) {
+        openLargeImagePopup(event.target.src);
+    }
+});
 
 // Gør funktionerne tilgængelige globalt
 window.hentPorteføljeEmner = hentPorteføljeEmner;
